4779e928fbec4457f6db2881f8edf69c28a147bd
Harbs1999 <jakeleeharbour@hotmail.com>
2021-11-01 02:48:15 +0000

    Added Timer, ability to play again and added styling

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+$("body").css("background-color", "black")
+// All Cards that will show on board
 const cards = [
     {
         name: "green goblin",
@@ -62,8 +64,10 @@ let selectedCards = [];
 let selectedCardsId = [];
 let correctCards = [];
 
+// Randomise card locations
 cards.sort(() => 0.5 - Math.random());
 
+//Add cards to board
 function setupBoard() {
     for (let i = 0; i < cards.length; i++) {
         let card = $(document.createElement('img'))
@@ -78,6 +82,7 @@ function setupBoard() {
 
 }
 
+//Flip Card functionality
 function flipCard() { 
     $("img").click(function () {
         let cardData = $(this).attr('data')
@@ -86,19 +91,21 @@ function flipCard() {
         $(this).attr('src', cards[cardData].img)
 
         if (selectedCards.length === 2) {
-            setTimeout(checkIfMatch, 600)
+            setTimeout(checkIfMatch, 200)
         }
     })
 }
 
+// Compare selected cards tp see if match /
+// If complete board tell player they win and start again
 function checkIfMatch() {
     const optionOneId = selectedCardsId[0]
     const optionTwoId = selectedCardsId[1]
     if (selectedCards[0] === selectedCards[1]) {
         alert("Correct!")
         correctCards.push(selectedCards)
-        $($("img")[optionOneId]).hide()
-        $($("img")[optionTwoId]).hide()  
+        $($("img")[optionOneId]).hide("slow")
+        $($("img")[optionTwoId]).hide("slow")  
     } else {
         $($("img")[optionOneId]).attr("src", "/static/images/marvel-card-back.jpg")
         $($("img")[optionTwoId]).attr("src", "/static/images/marvel-card-back.jpg")
@@ -106,12 +113,31 @@ function checkIfMatch() {
     }
     selectedCards = []
     selectedCardsId = []
+    if (correctCards.length === 7) {
+    alert("You got all cards!")
+    $("#play-again").html("<a href='index.html' class='btn btn-primary col-12'>Play Again</a>")
+}
+    $("#score").html(correctCards.length)
 }  
 
-
-
+// Sets timer and counts down to 0, ends game if either timer 0 or all cards cleared
+function timer() {
+let seconds = 30;
+const intervalId = setInterval(() => {
+  seconds -= 1;
+  $("#timer").html(seconds)
+if (seconds === 0) {
+    $("img").hide("fast");
+    $("#play-again").html("<a href='index.html' class='btn btn-primary col-12'>Play Again</a>")
+    clearInterval(intervalId);
+  }
+if (correctCards.length === 7) {
+    clearInterval(intervalId);
+}
+}, 1000);
+}
 
 setupBoard();
 flipCard();
-
+timer();
 });
